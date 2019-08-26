07a8c159917a5d2418d5d7ac765618e9a16ad963
Damyan Peykov <dppeykov@gmail.com>
2019-08-26 12:21:55 +0200

    Adding Redux
    Changing the search to go through Redux store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
+import { setSearchField } from "../actions";
 import "./App.css";
+
+const mapStateToProps = state => {
+  return { searchField: state.searchField };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchfield: ""
+      robots: []
     };
   }
 
@@ -22,21 +33,18 @@ class App extends Component {
       });
   }
 
-  onSearchChange = event => {
-    this.setState({ searchfield: event.target.value });
-  };
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     return !robots.length ? (
       <h1>Loading</h1>
     ) : (
       <div className="tc">
         <h1 className="f1 tracked">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots} />
@@ -47,4 +55,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
